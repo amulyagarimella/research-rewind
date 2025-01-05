4e41852e337b8dcc966e2f9dd8af514ac45b5849
Amulya Garimella <amulyagarimella@users.noreply.github.com>
2025-01-05 12:26:40 +1100

    lint

--- src/app/api/submitForm/route.ts
+++ src/app/api/submitForm/route.ts
@@ -35,6 +35,7 @@ export async function POST(request: Request) {
 
         return new Response(JSON.stringify({ success: true }), { status: 201 });
     } catch (error) {
+        console.log(error);
         return new Response(JSON.stringify({ success: false }), { status: 500 });
     }
 }
