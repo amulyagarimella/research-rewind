dc4aa618927f9a3b904d917a95cbc6397bc3aa54
Amulya Garimella <amulyagarimella@users.noreply.github.com>
2025-01-05 17:18:08 +1100

    email, unsubscribe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,14 @@ const subjects = domains["results"];
 
 const intervals = [1, 5, 10, 50, 100];
 
+console.log(process.env.NEXT_PUBLIC_BASE_URL);
+
 export default function App() {
   return (
     <div>
       <div className="flex flex-col items-center">
-        <h1>Vintage Nature</h1>
-        <p>Receive daily emails with the top Nature papers from the past.</p>
+        <h1>Research Rewind</h1>
+        <p>Receive daily emails with the top Nature papers from decades past.</p>
       
         <h2>Sign up</h2>
         <Form subjects={subjects} intervals={intervals}/>
