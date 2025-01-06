b45a8c0cd53bfc704a1bf2518ee4a00ab51c9f34
Amulya Garimella <amulyagarimella@users.noreply.github.com>
2025-01-06 13:52:56 +1100

    url

--- src/app/api/sendDailyEmail/route.ts
+++ src/app/api/sendDailyEmail/route.ts
@@ -22,7 +22,9 @@ function formatAuthors(authors:string[]) {
 }
 
 const getBaseUrl = () => {
-    if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview" || process.env.NEXT_PUBLIC_VERCEL_ENV === "production") {
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production") {
+        return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+    } else if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
         return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
     }
     return "http://localhost:3000";
